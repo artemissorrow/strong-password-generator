a0401887a204f15c32af6de6fbb5c719b74407be
Artemis Morrow <c.artemis.morrow@gmail.com>
2021-03-07 18:07:36 -0500

    even more documentation

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-console.log("testing...")
-
+// establishes variables that will be combined below for the randomize function to draw from
 var charSet = "";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
-// get password criteria from user, starting with length
+// gets password criteria from user, starting with length
 // if length is outside the desired range, it loops around and asks again
 function generatePassword() {
   window.charNum = prompt("How many characters does your password need?", "min. 8 - max. 128")
@@ -33,12 +32,12 @@ function generatePassword() {
     charSet += specialCharacters;
   }
 
-  //validate input - if nothig is selected above, error message displays
+  //validates input - if nothig is selected above, error message displays
   if (charSet == "") {
     alert("Error: password must contain letters, numbers, or special characters")
   }
 
-  //generate random string using input above - for loop runs through the 
+  //generates random string using input above - for loop runs through the 
   // string established above until the new string is the desired length
   function randomize(length) {
     window.result = "";
@@ -50,11 +49,11 @@ function generatePassword() {
 
   randomize(charNum);
 
-// reset the string so we can start from scratch without reloading
+// resets the string to start from scratch without reloading
   charSet = "";
 }
 
-// Write password to the #password input
+// Writes password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -66,7 +65,7 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Adds event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
