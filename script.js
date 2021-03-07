980e64512338585ab0e414223bf929edf8545fe0
Artemis Morrow <c.artemis.morrow@gmail.com>
2021-03-07 11:29:19 -0500

    password displays

--- script.js
+++ script.js
@@ -56,9 +56,10 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = result;
 
-}
+
+} 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
